152fdf2e64493cf9053dc1c671e17f6d76408787
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2018-03-26 20:09:14 +0200

    added editx challenge banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker'
+
+import { environment } from '../environments/environment';
 
 import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +22,7 @@ const PROVIDERS = [
 
 const IMPORTS = [
   BrowserModule.withServerTransition({appId: 'ngbe-app'}),
+  environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
   RouterModule.forRoot([
     ...AppRoutes
   ]),
