cd00d0627351f228e7a8e73edd78d1b009f61a0b
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2018-05-07 23:08:44 +0200

    sponsor page & styles

--- src/app/shared/components/organisers/organisers.component.ts
+++ src/app/shared/components/organisers/organisers.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ngbe-organisers',
@@ -8,6 +8,8 @@ import {Component} from '@angular/core';
   ]
 })
 export class OrganisersComponent {
+
+  @Input() banner = false;
 
   constructor() {
   }
