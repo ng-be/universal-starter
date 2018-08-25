ec5562de6f58015d2535b5e7da90dea59525029e
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2018-08-25 13:50:32 +0200

    removed service worker + update versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ServiceWorkerModule } from '@angular/service-worker';
-
-import { environment } from '../environments/environment';
+import {TransferHttpCacheModule} from '@nguniversal/common';
 
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
 import { HomeComponent } from './home/home.component';
-
 
 const COMPONENTS = [
   AppComponent,
@@ -22,11 +19,11 @@ const PROVIDERS = [
 
 const IMPORTS = [
   BrowserModule.withServerTransition({appId: 'ngbe-app'}),
-  ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
   RouterModule.forRoot([
     ...AppRoutes
   ]),
-  SharedModule
+  SharedModule,
+  TransferHttpCacheModule
 ];
 
 @NgModule({
